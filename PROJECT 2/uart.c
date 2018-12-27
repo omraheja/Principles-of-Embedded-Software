9770455e7e57e60b6450f3b699a18fc256ffc027
omraheja <43196196+omraheja@users.noreply.github.com>
2018-12-27 05:45:42 -0700

    Update uart.c

--- PROJECT 2/uart.c
+++ PROJECT 2/uart.c
@@ -29,17 +29,17 @@ int8_t rx_data;
 /*                                                  MACROS                                                  */
 /*----------------------------------------------------------------------------------------------------------*/
 
-#define ENABLE_CLOCK_UART0 			0x400u                    //Enable clock for UART0
-#define BDH_VALUE 					0x00                      //Baud rate value higher bits
-#define BDL_VALUE 					0x18                      //Baud rate value lower bits
-#define OSR_VALUE 					0x0F                      //Over sampling ratio/rate(OSR) value
-#define ENABLE_CLOCK_PORTA 			0x0200           		  //Enable clock for PORTA
-#define CLOCK_SELECT 				0x04000000 		    	  //Use FFL output for UART Baud rate generator
-#define TIE_FLAG 					0x80u                     //Enable Transmit Interrupt
-#define RIE_FLAG 					0x20u                     //Enable Receive Interrupt
-#define PIN_MUX_CTRL 				0x0200                    //Select Alternative 2
-#define TE_FLAG 					0x08                      //Enable Transmitter
-#define RE_FLAG 					0x04					  //Enable Receiver
+#define ENABLE_CLOCK_UART0				0x400u			//Enable clock for UART0
+#define BDH_VALUE					0x00			//Baud rate value higher bits
+#define BDL_VALUE					0x18			//Baud rate value lower bits
+#define OSR_VALUE					0x0F			//Over sampling ratio/rate(OSR) value
+#define ENABLE_CLOCK_PORTA				0x0200			//Enable clock for PORTA
+#define CLOCK_SELECT					0x04000000		//Use FFL output for UART Baud rate generator
+#define TIE_FLAG					0x80u			//Enable Transmit Interrupt
+#define RIE_FLAG					0x20u			//Enable Receive Interrupt
+#define PIN_MUX_CTRL					0x0200			//Select Alternative 2
+#define TE_FLAG						0x08			//Enable Transmitter
+#define RE_FLAG						0x04			//Enable Receiver
 
 /*----------------------------------------------------------------------------------------------------------*/
 /*                                            FUNCTION PROTOTYPES                                           */
